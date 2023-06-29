6cda92eaaf151444b4e47dee98247fda3ae84d59
Paul He <pauhe@student.pakuranga.school.nz>
2023-06-29 13:26:33 +1200

    29 2

--- app.py
+++ app.py
@@ -25,12 +25,38 @@ def create_connection():
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor
     )
+def can_access(id):
+    if "logged_in" in session:
+        matching_id = session["id"] == int(id)
+        is_admin = session["role"] == "admin"
+
+        return matching_id or is_admin
+    else:
+        return False
+
 
 @app.route("/")
 def home():
     
     return render_template("home.html")
 
+@app.route("/view")
+def view():
+    if not can_access(request.args["id"]):
+        flash("You don't have permission to do that!")
+        return redirect("/")
+    
+    else:
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM users WHERE id = %s"
+                values = (
+                    request.args["id"]
+                )
+                cursor.execute(sql, values)
+                result = cursor.fetchone()
+                return render_template("view.html", result=result)
+    
 @app.route("/user")
 def user():
     
@@ -49,51 +75,7 @@ def user():
         
         with create_connection() as connection:
                 return render_template("view.html",)
-    """
-    elif session["role"] == "user":
-        with create_connection() as connection:
-            with connection.cursor() as cursor:
-                sql = "SELECT * FROM users WHERE id = %s"
-                values = (
-                    request.args["id"]
-                    )
-                cursor.execute(sql, values)
-                result = cursor.fetchall()
-        return render_template("user.html", result=result)
-"""
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        with create_connection() as connection:
-            with connection.cursor() as cursor:
-                sql = "SELECT * FROM users WHERE email = %s AND password = %s"
-                values = (
-                    request.form["email"],
-                    encrypt(request.form["password"])
-                )
-                cursor.execute(sql, values)
-                result = cursor.fetchone()
-        if result:
-            session["logged_in"] = True
-            session["id"] = result["id"]
-            session["first_name"] = result["first_name"]
-            session["last_name"] = result["last_name"]
-            session["profile"] = result["profile"]
-            session["email"] = result["email"]
-            session["role"] = result["role"]
-            return redirect("/")
-        else:
-            flash("Wrong username or password!")
-            return redirect("/login")
-    return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect("/")
-
-
+    
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -126,6 +108,38 @@ def signup():
         return redirect("/")
     else:
         return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+                values = (
+                    request.form["email"],
+                    encrypt(request.form["password"])
+                )
+                cursor.execute(sql, values)
+                result = cursor.fetchone()
+        if result:
+            session["logged_in"] = True
+            session["id"] = result["id"]
+            session["first_name"] = result["first_name"]
+            session["last_name"] = result["last_name"]
+            session["profile"] = result["profile"]
+            session["email"] = result["email"]
+            session["role"] = result["role"]
+            return redirect("/")
+        else:
+            flash("Wrong username or password!")
+            return redirect("/login")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
 
 @app.route("/delete")
 def delete():
@@ -195,33 +209,8 @@ def update():
                 result = cursor.fetchone()
         return render_template("update.html", result=result)
 
-def can_access(id):
-    if "logged_in" in session:
-        matching_id = session["id"] == int(id)
-        is_admin = session["role"] == "admin"
-
-        return matching_id or is_admin
-    else:
-        return False
 
 
-@app.route("/view")
-def view():
-    if not can_access(request.args["id"]):
-        flash("You don't have permission to do that!")
-        return redirect("/")
-    
-    else:
-        with create_connection() as connection:
-            with connection.cursor() as cursor:
-                sql = "SELECT * FROM users WHERE id = %s"
-                values = (
-                    request.args["id"]
-                )
-                cursor.execute(sql, values)
-                result = cursor.fetchone()
-                return render_template("view.html", result=result)
-    
 
 
 app.run(debug=True)
