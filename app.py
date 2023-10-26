5d9c3661bc22e00e13066635929a270508d0d5e6
Paul He <pauhe@student.pakuranga.school.nz>
2023-10-26 13:25:46 +1300

    Comments added

--- app.py
+++ app.py
@@ -9,30 +9,29 @@ from flask import Flask, render_template, request, redirect, session, flash, abo
 app = Flask(__name__)
 app.secret_key = "any-random-string-reshrdjtfkygluvchfjkhlbh"
 
+# 404 page if error occur
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template("404.html")
 
+# Encrypt password
 def encrypt(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-
+# Connection to the database
 def create_connection():
-    
     return pymysql.connect(              
         host="10.0.0.17",
         user="pauhe",
-        # host="localhost",
-        # user="root",
         password="ALTAR",
         db="pauhe_lost&found",
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor
     )
 
-
+# Check if users can access a page or not
 def can_access(id):
-    if "logged_in" in session and id.isdigit():
+    if "logged_in" in session or id.isdigit():
         matching_id = session["id"] == int(id)
         is_admin = session["role"] == "admin"
 
@@ -52,7 +51,6 @@ def can_accesslost(post_id, session_user_id):
             if not result:
                 return False  
             # The post doesn't exist
-
             post_user_id = result["userid"]
 
     is_admin = session.get("role") == "admin"
@@ -60,26 +58,31 @@ def can_accesslost(post_id, session_user_id):
 
 
 
-
+# Home page
 @app.route("/")
 def home():
-    
     return render_template("home.html")
 
+# View sigle users details
 @app.route("/view")
 def view():
+    if  not "logged_in" in session:
+        flash("Please log in")
+        return redirect((request.referrer or "/") if request.referrer != "404.html" else "/")  
     if session["role"] == "admin":
         try:
             id = request.args["id"]
         except KeyError:
             flash("Please log in")
             return redirect((request.referrer or "/") if request.referrer != "404.html" else "/")
+        # If admin is viewing, then the user they wanted to see will be displayed
     elif session["role"] == "user":
         try:
             id = session["id"]
         except KeyError:
-            flash("Please log in")
+            flash("You don't have permission to do that!")
             return redirect((request.referrer or "/") if request.referrer != "404.html" else "/")
+        # If user is viewing, it will display their information
     if not can_access(id):
         flash("Please log in")
         return redirect((request.referrer or "/") if request.referrer != "404.html" else "/")  
@@ -88,16 +91,14 @@ def view():
         with create_connection() as connection:
             with connection.cursor() as cursor:
                 sql = "SELECT * FROM users WHERE id = %s"
-                values = (
-                    id
-                )
-                cursor.execute(sql, values)
+                cursor.execute(sql, id)
                 result = cursor.fetchone()
                 return render_template("view.html", result=result)
     
+
+# Admin page to view all users
 @app.route("/user")
 def user():
-    
     if not "logged_in" in session:
         flash("Please log in")
         return redirect((request.referrer or "/") if request.referrer != "404.html" else "/")  
@@ -112,6 +113,7 @@ def user():
     else:
         return redirect("/view")
     
+# Sigh up page
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -133,11 +135,11 @@ def signup():
                     ext = os.path.splitext(profile.filename)[1]
                     profile_path = "static/images/" + str(uuid.uuid4())[:8] + ext
                     profile.save(profile_path)
-                    
+                # If no profile was given, there will be a defalt one
+                # If there is, there will be different names for everything
+
                 password = request.form["password"]
                 encrypted_password = hashlib.sha256(password.encode()).hexdigest()
-                
-
                 sql = """INSERT INTO users (first_name, last_name, email, password, profile)
                 VALUES (%s, %s, %s, %s, %s)"""
                 values = (
@@ -153,6 +155,7 @@ def signup():
     else:
         return render_template("signup.html")
 
+# Log in page
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -179,12 +182,13 @@ def login():
             return redirect("/login")
     return render_template("login.html")
 
+# Log out page
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
 
-
+# Delete user page
 @app.route("/delete")
 def delete():
     try:
@@ -203,6 +207,7 @@ def delete():
             connection.commit()
     return redirect("/user")
 
+# Update user's detil
 @app.route("/update", methods=["GET", "POST"])
 def update():
     if not can_access(request.args["id"]):
@@ -220,14 +225,14 @@ def update():
                     encrypted_password = hashlib.sha256(password.encode()).hexdigest()
                 else:
                     encrypted_password = request.form["old_password"]
-
+                # If no password givin, the old password will be saved
                 if profile: 
                     ext = os.path.splitext(profile.filename)[1]
                     profile_path = "static/images/" + str(uuid.uuid4())[:8] + ext
                     profile.save(profile_path)
                 else:
                     profile_path = request.form["old_profile"]
-
+                # Same as profile
                 sql = """UPDATE USERS SET
                     first_name = %s,
                     last_name = %s,
@@ -248,6 +253,7 @@ def update():
                 cursor.execute(sql, values)
                 connection.commit()
         return redirect("/logout")
+        # Make user log out after, so the details could be updated
     else:
         with create_connection() as connection:
             with connection.cursor() as cursor:
@@ -257,8 +263,8 @@ def update():
                 result = cursor.fetchone()
         return render_template("update.html", result=result)
 
-#lost
 
+# Lost page
 @app.route("/lost")
 def lost():
 
@@ -287,8 +293,9 @@ def lost():
                 lost_results = cursor.fetchall()
 
             return render_template("lostadmin.html", lost_results=lost_results)
-    
+            # Admin page that have more functions
 
+# Delete post
 @app.route("/deletepost")
 def deletepost():
     try:
@@ -309,6 +316,7 @@ def deletepost():
 
     return redirect("/lost")
 
+# Update post
 @app.route("/updatepost", methods=["GET", "POST"])
 def updatepost():
     post_id = request.args.get("id")
@@ -356,6 +364,7 @@ def updatepost():
                 result = cursor.fetchone()
         return render_template("updatepost.html", result=result)
 
+# View posts in details
 @app.route("/viewpost")
 def viewpost():
     try:
@@ -380,6 +389,7 @@ def viewpost():
                 
             return render_template("viewpost.html", lost_result=lost_result)
 
+# Post something found 
 @app.route("/post", methods=["GET", "POST"])
 def post():
     if not "logged_in" in session:
@@ -389,7 +399,6 @@ def post():
     if request.method == "POST":
         with create_connection() as connection:
             with connection.cursor() as cursor:
-
                 image = request.files["image"]
                 if not image:
                     image_path = "static/images/profile.png"
